c470d5d2d6cd45c341be612284c0ec676228f20e
Jarelle <48391432+jarelleboac@users.noreply.github.com>
2021-09-03 16:59:22 -0400

    update bug fixes and dynamic content in email

--- src.py
+++ src.py
@@ -1,51 +1,51 @@
 import pandas as pd
 import random as rd
+
 #email configuration
 import smtplib
 from email.message import EmailMessage
 
 EMAIL_ADDRESS = 'ruwicsbuddies@gmail.com'
-EMAIL_PASSWORD = 'INSERT PASSWORD HERE'
+EMAIL_PASSWORD = 'uclawgezoarwszbq'
+
 
 
 col_list = ["Email address", "Full Name"]
-df = pd.read_csv("names_modd.csv", usecols=col_list)
+df = pd.read_csv("t1.csv", usecols=col_list)
 df_list=df.values.tolist() #converts dataframe to list
-
+copy_df_list= df_list
 num_pairs=len(df.index)/2
 pairs_list= list() #list of pairs
 
 while df_list:
     if len(df_list) ==3: #odd number of participants
-        x=rd.choice(df_list)
-        df_list.remove(x)
-        y=rd.choice(df_list)
-        df_list.remove(y)
-        z=rd.choice(df_list)
-        pair= [x,y,z]
+        pair= rd.sample(df_list, 3)
+        #print("item of 3 is:", pair)
+        pairs_list.append(pair)
         break        
     else:
-        x=rd.choice(df_list)
-        df_list.remove(x)
-        y=rd.choice(df_list)
-        df_list.remove(y)
-        pair= [x,y]
-
+        pair= rd.sample(df_list, 2)
         if pair not in pairs_list: #avoid duplicates by checking if pair is created
             pairs_list.append(pair)
-        else:
-            df_list.append(x)
-            df_list.append(y)
+            for row in pair:
+                    #print("item is:", item)
+                    df_list.remove(row)
 
-#TODO: 1) extract first names and last names 2) extract email 3) send to email
+for pair in pairs_list: 
+    #print("pair:", row)
+    for individual in pair:
+        print("individual:", individual)
+        msg = EmailMessage() #instantiate email message class to send email
+        msg['Subject'] = 'WiCS Buddy Bot Pair!'
+        msg['From'] = EMAIL_ADDRESS 
+        msg['To'] = [individual[0]]  #email will always be individual[0]
+        
+        #content of email
+        html="""Hi, $(name)"""
+        html = html.replace("$(name)", individual[1])
+        msg.set_content(html)
 
-msg = EmailMessage()
-msg['Subject'] = 'This is my test email for multiple people'
-msg['From'] = EMAIL_ADDRESS 
-msg['To'] = ['recipient1, 'recipient2']
-msg.set_content('it works')
 
-
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD) 
-    smtp.send_message(msg)
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD) 
+            smtp.send_message(msg)
