d9f9648ff1389571ef4400e29332070219181da6
Jarelle <48391432+jarelleboac@users.noreply.github.com>
2022-01-25 13:08:11 -0500

    updated with even participant conditional

--- round_robin.py
+++ round_robin.py
@@ -23,29 +23,27 @@ def rotate(top_array, bottom_array):
     return top_array, bottom_array
 
 def main():
+
+    test= [1 ,2, 3, 4, 8, 7, 6, 5]
     col_list = ["Email address", "Full Name"] #extracting only the important values (for now): email, full name
     df = pd.read_csv("form_responses.csv", usecols=col_list) #reading in the form responses exported to csv into a dataframe
     participants =df.values.tolist() #converts dataframe of form responses to list
     current_week_pairs = list() #list to store this week's pairs in
 
-    if path.exists('updated_pair_combinations.csv'):
-        print("it exists!")
-    else:
-        if len(participants) == 0:
-            print("no one has signed up. Pairs are not possible at this time")
-            return
-        elif len(participants) == 1:
-            print("only one person has signed up. Pairs are not possible at this time")
-            return
-        elif len(participants) % 2 == 0: #even number of participants
-            print("even number of participants")
+    if len(participants) == 0:
+        print("no one has signed up. Pairs are not possible at this time")
+        return
+    elif len(participants) == 1:
+        print("only one person has signed up. Pairs are not possible at this time")
+        return
+    elif len(participants) % 2 == 0: #even number of participants
+        #partition participants into two lists            
+        top_array, bottom_array = split_list(participants) 
+        top_array, bottom_array = rotate(top_array, bottom_array)
 
-        else: #odd number of participants
-            #partition participants into two lists            
-            top_array, bottom_array = split_list(participants) 
-            top_array.append(0) #top array will need a temporary holder of 0, the 0 will indicate that this person does not receive a pair for the week
-            top_array, bottom_array = rotate(top_array, bottom_array)
+    else: #odd number of participants
+        #partition participants into two lists            
+        top_array, bottom_array = split_list(participants) 
+        top_array.append(0) #top array will need a temporary holder of 0, the 0 will indicate that this person does not receive a pair for the week
+        top_array, bottom_array = rotate(top_array, bottom_array)
 
-
-if __name__ == "__main__":
-    main()
